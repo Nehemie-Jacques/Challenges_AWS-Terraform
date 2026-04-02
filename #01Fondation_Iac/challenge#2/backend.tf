69b30d41361b0cba6dbec93d8f915cbb93e8ae61
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-04-02 15:55:23 +0100

    output & end challenge2

--- #01Fondation_Iac/challenge#2/backend.tf
+++ #01Fondation_Iac/challenge#2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "bucket-bootstrap-challenge1"
-    key            = "./terraform.tfstate"
+    key            = "challenge-02/terraform.tfstate"
     region         = "eu-west-3"
     dynamodb_table = "terraform-locks"
     encrypt        = true
