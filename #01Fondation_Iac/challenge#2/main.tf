1cc62a03ddc3b1f843a23b06818a84bacb2c4820
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-04-02 09:07:28 +0100

    create vpc & public subnet

--- #01Fondation_Iac/challenge#2/main.tf
+++ #01Fondation_Iac/challenge#2/main.tf
@@ -1,13 +1,48 @@
-resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block
-  instance_tenancy = "default"
-  
-  tags = var.tags
-
-  enable_dns_hostnames = true
-  enable_dns_support = true
-}
-
 data "aws_availability_zones" "available" {
   state = "available"
+}
+
+locals {
+  selected_azs = slice(data.aws_availability_zones.available.names, 0, var.az_count)
+
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+
+  public_subnets = {
+    for i, az in locals.selected_azs :
+    az => cidrsubnet(var.vpc_cidr_block, var.subnet_newbits, i)
+  }
+
+  private_subnets = {
+    for i, az in locals.selected_azs :
+    az => cidrsubnet(var.vpc_cidr_block, var.subnet_newbits, i + var.az_count)
+  }
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = merge(local.common_tags, {
+    name = "${var.project}-${var.environment}-vpc"
+  })
+}
+
+resource "aws_subnet" "public" {
+  for_each = local.public_subnets
+
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value
+  availability_zone = each.key
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, {
+    name = "${var.project}-${var.environment}-public-subnet-${each.key}"
+    Tier = "Public"
+  })
 }
--- #01Fondation_Iac/challenge#2/variables.tf
+++ #01Fondation_Iac/challenge#2/variables.tf
@@ -4,18 +4,32 @@ variable "aws_region" {
   default     = "eu-west-3"
 }
 
-variable "cidr_block" {
+variable "project" {
+  description = "The project name"
+  type        = string
+  default     = "challenge2"
+}
+
+variable "environment" {
+  description = "The environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-variable "tags" {
-  description = "Tags for the VPC"
-  type        = map(string)
-  default     = {
-    Name = "main"
-    project = "challenge2"
-    environment = "dev"
-  }
+variable "az_count" {
+  description = "The number of availability zones"
+  type        = number
+  default     = 2
+}
+
+variable "subnet_newbits" {
+  description = "Additional prefix bits to create subnets from VPC CIDR"
+  type = number
+  default = 8
 }
