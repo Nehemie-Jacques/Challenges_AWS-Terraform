e6ecb22b44bac2f6acb91a4a349297da55f0dacb
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-04-06 20:23:08 +0100

    correction d'erreur

--- #01Fondation_Iac/challenge#4/root/outputs.tf
+++ #01Fondation_Iac/challenge#4/root/outputs.tf
@@ -1,7 +1,7 @@
 output "dev_website_url" {
-  value = module.static_site_dev.website_url
+  value = module.static_site_dev.url_web_site
 }
 
 output "prod_website_url" {
-  value = module.static_site_prod.website_url
+  value = module.static_site_prod.url_web_site
 }
