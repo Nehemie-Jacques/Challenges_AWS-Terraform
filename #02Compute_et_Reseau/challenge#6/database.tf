a44e2ef3ae0c8edb2372c0ad460092aefdb9a88e
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-04-09 19:53:46 +0100

    RDS

---
+++ #02Compute_et_Reseau/challenge#6/database.tf
@@ -0,0 +1,19 @@
+resource "aws_db_subnet_group" "main" {
+  name       = "${var.project}-${var.environment}-db-subnet-group"
+  subnet_ids = [for s in aws_subnet.private : s.id]
+}
+
+resource "aws_db_instance" "main" {
+ identifier             = "${var.project}-${var.environment}-db"
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_name                = var.db_name
+  username               = "admin"
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.sg_rds.id]
+  skip_final_snapshot    = true
+  multi_az               = false
+}
