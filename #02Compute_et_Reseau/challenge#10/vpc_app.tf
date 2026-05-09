df8d6ce6dcf88c16c915c6e1a2bd76de278c4687
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-05-10 00:29:39 +0100

    perf(challenge#10) : Ajout des IGW, des routes tables et association dans les deux vpcs

--- #02Compute_et_Reseau/challenge#10/vpc_app.tf
+++ #02Compute_et_Reseau/challenge#10/vpc_app.tf
@@ -1,34 +1,50 @@
 resource "aws_vpc" "vpc_app" {
-  cidr_block = var.vpc_cidr_block-app
-  tags = {
-    Name = "VPC-App"
-  }
+  cidr_block           = var.vpc_cidr_block-app
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = { Name = "VPC-App" }
 }
 
 resource "aws_subnet" "subnet_app" {
   vpc_id                  = aws_vpc.vpc_app.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = cidrsubnet(aws_vpc.vpc_app.cidr_block, 8, 1)
   map_public_ip_on_launch = true
+  tags                    = { Name = "Subnet-App" }
+}
 
-  tags = {
-    Name = "Subnet-App"
+resource "aws_internet_gateway" "igw_app" {
+  vpc_id = aws_vpc.vpc_app.id
+  tags   = { name = "IGW-App" }
+}
+
+resource "aws_route_table" "rt_app" {
+  vpc_id = aws_vpc.vpc_app.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_app.id
   }
+  tags = { Name = "RT-App" }
+}
+
+resource "aws_route_table_association" "rta_app" {
+  subnet_id      = aws_subnet.subnet_app.id
+  route_table_id = aws_route_table.rt_app.id
 }
 
 resource "aws_security_group" "allow_icmp_cross_vpc" {
   name        = "allow_icmp_from_other_vpc"
-  description = "Autorise le trafic ICMP entrant depuis le VPC distant"
+  description = "Autorise le trafic ICMP entrant depuis le VPC tools"
   vpc_id      = aws_vpc.vpc_app.id
 
-  ingress = {
-    description = "Trafic ICMP entrant depuis le VPC distant"
+  ingress {
+    description = "Trafic ICMP entrant depuis le VPC tools"
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
     cidr_blocks = [aws_vpc.vpc_tools.cidr_block]
   }
 
-  egress = {
+  egress {
     description = "Autorisation de tout le trafic sortant"
     from_port   = 0
     to_port     = 0
@@ -36,14 +52,14 @@ resource "aws_security_group" "allow_icmp_cross_vpc" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "allow_icmp_cross_vpc"
-  }
+  tags = { Name = "allow_icmp_cross_vpc" }
 }
 
 resource "aws_instance" "app_instance" {
-  ami                    = "ami-0c7191dc6b3e08b56"
-  instance_type          = "t3.micro"
+  ami                    = "data.aws_ami.amazon_linux_2.id"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet_app.id
   vpc_security_group_ids = [aws_security_group.allow_icmp_cross_vpc.id]
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  tags                   = { Name = "App-Instance" }
 }
--- #02Compute_et_Reseau/challenge#10/vpc_tools.tf
+++ #02Compute_et_Reseau/challenge#10/vpc_tools.tf
@@ -1,18 +1,34 @@
 resource "aws_vpc" "vpc_tools" {
-  cidr_block = var.vpc_cidr_block-tools
-  tags = {
-    Name = "VPC-Tools"
-  }
+  cidr_block           = var.vpc_cidr_block-tools
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags                 = { Name = "VPC-Tools" }
 }
 
 resource "aws_subnet" "subnet_tools" {
   vpc_id                  = aws_vpc.vpc_tools.id
-  cidr_block              = "10.1.1.0/24"
+  cidr_block              = cidrsubnet(aws_vpc.vpc_tools.cidr_block, 8, 2)
   map_public_ip_on_launch = true
+  tags                    = { Name = "Subnet-Tools" }
+}
 
-  tags = {
-    Name = "Subnet-Tools"
+resource "aws_internet_gateway" "igw_tools" {
+  vpc_id = aws_vpc.vpc_tools.id
+  tags   = { name = "IGW-Tools" }
+}
+
+resource "aws_route_table" "rt_tools" {
+  vpc_id = aws_vpc.vpc_tools.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_tools.id
   }
+  tags = { Name = "RT-Tools" }
+}
+
+resource "aws_route_table_association" "rta_tools" {
+  subnet_id      = aws_subnet.subnet_tools.id
+  route_table_id = aws_route_table.rt_tools.id
 }
 
 resource "aws_security_group" "allow_icmp_cross_vpc" {
@@ -20,7 +36,7 @@ resource "aws_security_group" "allow_icmp_cross_vpc" {
   description = "Autorise le trafic ICMP entrant depuis le VPC distant"
   vpc_id      = aws_vpc.vpc_tools.id
 
-  ingress = {
+  ingress {
     description = "Trafic ICMP entrant depuis le VPC distant"
     from_port   = -1
     to_port     = -1
@@ -28,7 +44,7 @@ resource "aws_security_group" "allow_icmp_cross_vpc" {
     cidr_blocks = [aws_vpc.vpc_app.cidr_block]
   }
 
-  egress = {
+  egress {
     description = "Autorisation de tout le trafic sortant"
     from_port   = 0
     to_port     = 0
@@ -36,14 +52,14 @@ resource "aws_security_group" "allow_icmp_cross_vpc" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "allow_icmp_cross_vpc"
-  }
+  tags = { Name = "allow_icmp_cross_vpc" }
 }
 
 resource "aws_instance" "tools_instance" {
-  ami                    = "ami-0c7191dc6b3e08b56"
-  instance_type          = "t3.micro"
+  ami                    = "data.aws_ami.amazon_linux_2.id"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet_tools.id
   vpc_security_group_ids = [aws_security_group.allow_icmp_cross_vpc.id]
+  iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+  tags                   = { Name = "Tools-Instance" }
 }
