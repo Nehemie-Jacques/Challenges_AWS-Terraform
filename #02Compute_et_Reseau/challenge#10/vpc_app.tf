256676ab73b7bb96a5cab0d72221ddaac6f84b9d
Nehemie-Jacques <nehemiesighe1@gmail.com>
2026-05-09 14:36:29 +0100

    feat(challenge#10) : Déploiement d'une instance EC2 dans chaque VPC

--- #02Compute_et_Reseau/challenge#10/vpc_app.tf
+++ #02Compute_et_Reseau/challenge#10/vpc_app.tf
@@ -4,3 +4,46 @@ resource "aws_vpc" "vpc_app" {
     Name = "VPC-App"
   }
 }
+
+resource "aws_subnet" "subnet_app" {
+  vpc_id                  = aws_vpc.vpc_app.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Subnet-App"
+  }
+}
+
+resource "aws_security_group" "allow_icmp_cross_vpc" {
+  name        = "allow_icmp_from_other_vpc"
+  description = "Autorise le trafic ICMP entrant depuis le VPC distant"
+  vpc_id      = aws_vpc.vpc_app.id
+
+  ingress = {
+    description = "Trafic ICMP entrant depuis le VPC distant"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc_tools.cidr_block]
+  }
+
+  egress = {
+    description = "Autorisation de tout le trafic sortant"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_icmp_cross_vpc"
+  }
+}
+
+resource "aws_instance" "app_instance" {
+  ami                    = "ami-0c7191dc6b3e08b56"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.subnet_app.id
+  vpc_security_group_ids = [aws_security_group.allow_icmp_cross_vpc.id]
+}
--- #02Compute_et_Reseau/challenge#10/vpc_tools.tf
+++ #02Compute_et_Reseau/challenge#10/vpc_tools.tf
@@ -4,3 +4,46 @@ resource "aws_vpc" "vpc_tools" {
     Name = "VPC-Tools"
   }
 }
+
+resource "aws_subnet" "subnet_tools" {
+  vpc_id                  = aws_vpc.vpc_tools.id
+  cidr_block              = "10.1.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Subnet-Tools"
+  }
+}
+
+resource "aws_security_group" "allow_icmp_cross_vpc" {
+  name        = "allow_icmp_from_other_vpc"
+  description = "Autorise le trafic ICMP entrant depuis le VPC distant"
+  vpc_id      = aws_vpc.vpc_tools.id
+
+  ingress = {
+    description = "Trafic ICMP entrant depuis le VPC distant"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.vpc_app.cidr_block]
+  }
+
+  egress = {
+    description = "Autorisation de tout le trafic sortant"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_icmp_cross_vpc"
+  }
+}
+
+resource "aws_instance" "tools_instance" {
+  ami                    = "ami-0c7191dc6b3e08b56"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.subnet_tools.id
+  vpc_security_group_ids = [aws_security_group.allow_icmp_cross_vpc.id]
+}
